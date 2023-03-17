d6c5c4abad9f82846fe5d4529bb13d5c37204a27
RamyBadr1 <RamyBadr054@gmail.com>
2023-03-17 08:47:09 -0700

    4.puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main() 
+
+/**
+ * Using puts() to print something.
+*/
+
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;	
+	(return 0);	
 }
