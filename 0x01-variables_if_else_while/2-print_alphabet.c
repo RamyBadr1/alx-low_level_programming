4d88172f067cd5d331fbd024e45f0e53a8a6ef90
RamyBadr1 <ramybadr054@gmail.com>
2023-03-18 04:19:52 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
  * Return: Always 0 (SUCCESS)
 */
 int main(void) {
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < strlen(alphabet); i++)
+	for (i = 0; i < 26; i++)
 		putchar(alphabet[i]);
 
 	printf("\n");
