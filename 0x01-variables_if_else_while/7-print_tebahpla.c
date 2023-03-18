f83b05c6028e8abd3c88af10d67d78f1732960f9
RamyBadr1 <ramybadr054@gmail.com>
2023-03-18 16:44:40 +0200

    6-print_number.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main-Entry Point.
+*
+* Return: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+	char chars;
+	for (chars = 'z'; chars == 'a'; chars--)
+		putchar(chars);
+
+	putchar('\n');
+
+	return (0);
+}
