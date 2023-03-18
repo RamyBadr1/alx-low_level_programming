6de19d063eb9b69dc98c7833dd4c5bbbfac07811
RamyBadr1 <ramybadr054@gmail.com>
2023-03-18 05:37:51 +0200

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-Entry Point.
+ *
+ * Return: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+	char chars;
+
+	for (chars = 'a'; chars <= 'z'; chars++)
+	{
+		if (chars == 'e' || chars == 'q')
+			continue;
+		
+		putchar(chars); 
+	}
+
+	printf("\n");
+	
+	return (0);
+}
