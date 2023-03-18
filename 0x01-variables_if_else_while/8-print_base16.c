87ce914a9b75ea7121834da72eb057b6c184c6cf
RamyBadr1 <ramybadr054@gmail.com>
2023-03-18 18:55:34 +0200

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main-Prints from 0 to 9.
+*
+* Return: Always 0 (SUCCESS).
+*/
+int main(void)
+{
+        int i;
+
+        for (i = 48; i <= 57; i++)
+                putchar(i);
+
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+
+        putchar('\n');
+
+        return (0);
+}
+
