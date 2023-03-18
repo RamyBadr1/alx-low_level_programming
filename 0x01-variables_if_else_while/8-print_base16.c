bb1bfec6f66844992cc3347492928b93ea4a83b8
RamyBadr1 <ramybadr054@gmail.com>
2023-03-18 19:03:13 +0200

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
 */
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 48; i <= 57; i++)
-                putchar(i);
+	for (i = 48; i <= 57; i++)
+		putchar(i);
 
 	for (i = 97; i <= 102; i++)
 		putchar(i);
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
 
