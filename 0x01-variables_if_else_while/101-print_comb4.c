9f63baab41a147e7a8aed27d21fe47ef90384a0f
RamyBadr1 <ramybadr054@gmail.com>
2023-03-20 18:53:11 +0200

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main-Entry Point.
+*
+* Return: Always 0 (SUCCESS)
+*/
+
+int main(void) {
+	int a;
+	int b;
+	int c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if(a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56 || c != 57)
+					{	
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
