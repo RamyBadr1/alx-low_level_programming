24ae8a7c3d78403e776bb767517f846250d66acf
RamyBadr1 <ramybadr054@gmail.com>
2023-03-20 17:44:46 +0200

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,20 +5,22 @@
 * Return: Always 0 (SUCCESS)
 */
 
-int main(void) {
+int main(void)
+{
 	int a;
 	int b;
-	
+
 	for (a = 48; a <= 56; a++)
-	{  	
+	{
 		for (b = 49; b <= 57; b++)
 		{
 			if (b > a)
 			{
 				putchar(a);
 				putchar(b);
-				
-				if (a != 56 || b != 57) {
+
+				if (a != 56 || b != 57)
+				{
 					putchar(',');
 					putchar(' ');	
 				}	
