754c5387cb23564015ea8e900cd52975dc377784
RamyBadr1 <ramybadr054@gmail.com>
2023-03-20 18:58:04 +0200

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,8 @@
 * Return: Always 0 (SUCCESS)
 */
 
-int main(void) {
+int main(void)
+{
 	int a;
 	int b;
 	int c;
@@ -17,11 +18,12 @@ int main(void) {
 		{
 			for (c = 50; c < 58; c++)
 			{
-				if(a < b && b < c)
+				if (a < b && b < c)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
+
 					if (a != 55 || b != 56 || c != 57)
 					{	
 						putchar(',');
