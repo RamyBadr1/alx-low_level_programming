dcb8332d68b896d5e9195a815d642955ece7c527
RamyBadr1 <ramybadr054@gmail.com>
2023-03-22 00:32:36 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-
-/**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0 (Success)
-*/
-
-int main(void) {
-	print_alphabet();
-	return (0);
-}
 
 /**
 * print_alphabet: prints alphabets using _putchar
