ede0af9e2ff6b99bfbe11bf345b8ab02030168d9
RamyBadr1 <ramybadr054@gmail.com>
2023-03-21 22:21:00 +0200

    putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_putchar - print c to stdout.
+*
+*Return on success 1.
+*On error, -1 is returned, and errno is set apprpriately.
+*/
+
+int _putchar()
+{
+	printf("_putchar");
+	return(0);
+}
