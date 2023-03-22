a014c2a9acbcc68570ecb88081468b4ef7b15886
RamyBadr1 <ramybadr054@gmail.com>
2023-03-22 02:22:44 +0200

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/*
+* Prints all alphabets 10 times.
+*/
+
+void print_alphabet_x10(void) {
+	int lines;
+	char letters;
+
+	for (lines = 0; lines < 10; lines++)
+	{
+		for (letters = 'a'; letters < 'z'; letters++)
+			_putchar++
+	}
+}
