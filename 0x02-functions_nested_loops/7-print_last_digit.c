912a2939270cd0d9591b32384e38a8f9f5a64c15
RamyBadr1 <ramybadr054@gmail.com>
2023-03-22 22:04:49 +0200

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_last_digit - calculate the remainder of (num) / 10
+* @n: int value to get its last digit
+* Return: return the remainder of (num) / 10
+*/
+
+int print_last_digit(int num) {
+	return (num % 10);
+}
+
