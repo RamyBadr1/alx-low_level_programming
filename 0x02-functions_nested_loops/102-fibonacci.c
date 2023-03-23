9db393f812464d6c7d9c77a4529ad0319973cc6c
RamyBadr1 <ramybadr054@gmail.com>
2023-03-23 03:41:53 +0200

    101-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 50; )
+	{
+		if (num == 1)
+		{
+			num += num;
+			continue;
+		{
+
+		num += num - 1;
+		printf("%d, ", num);
+	}
+
+	return (0);
+}
