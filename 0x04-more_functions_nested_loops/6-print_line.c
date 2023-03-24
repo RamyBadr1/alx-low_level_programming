7d14eab26be853f739c535fde497cce43f3c282f
RamyBadr1 <ramybadr054@gmail.com>
2023-03-24 23:09:25 +0200

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h";
+/**
+* print_line - print '_'
+* @n: int value
+*/
+
+void print_line(int n)
+{
+	int loop;
+
+	for (loop = 0; loop < n; loop++)
+		_putchar('_');
+
+	_putchar('\n');
+}
