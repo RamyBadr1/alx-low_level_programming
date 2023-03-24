a0582a9b613e6e064fa9c1a9a3dc66045f915e47
RamyBadr1 <ramybadr054@gmail.com>
2023-03-24 04:51:57 +0200

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <ctype.h>
+/**
+* _isdigit - checks if char is number
+* @c: A char value to be checked
+* Return: 1 if number - 0 if not
+*/
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	return (0);
+}
