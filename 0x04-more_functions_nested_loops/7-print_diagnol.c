10404218a16dc264a8f751a2e1b3fa2a8541cedb
RamyBadr1 <ramybadr054@gmail.com>
2023-03-25 01:25:53 +0200

    7-print_diagnol.

--- 0x04-more_functions_nested_loops/7-print_diagnol.c
+++ 0x04-more_functions_nested_loops/7-print_diagnol.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_diagonal - print number of spaces then '\'
 * @n: used to print number of spaces
@@ -7,7 +6,13 @@
 void print_diagonal(int n)
 {
 	int x, y;
-
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+		
 	for (x = 0; x < n; x++)
 	{
 		for (y = 0; y < x; y++)
