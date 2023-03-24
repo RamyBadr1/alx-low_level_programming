5878747faf3f482777bbd29c71bc0f10eb8f2d31
RamyBadr1 <ramybadr054@gmail.com>
2023-03-24 18:19:26 +0200

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_most_numbers - Print from 0 to 9 except 2 & 4
+*/
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		if (num != 50 || num != 52)
+			_putchar(num);
+		_putchar('\n');
+	}
+}
