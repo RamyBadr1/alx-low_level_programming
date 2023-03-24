a5b4d3281b76c7d73b4388627e8f9c1e99b6e76c
RamyBadr1 <ramybadr054@gmail.com>
2023-03-24 21:15:02 +0200

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* more_numbers - print numbers from 0 to 14.
+*/
+void more_numbers(void)
+{
+	int loops;
+	int num;
+
+	for (loops = 0; loops < 10; loops++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+				continue;
+			}
+			printf("%c", num + '0');
+		}
+		_putchar('\n');
+	}
+}
