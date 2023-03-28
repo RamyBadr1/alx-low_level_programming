0970c66e31bb5f1edde36970f67dc39b74821329
RamyBadr1 <ramybadr054@gmail.com>
2023-03-28 22:11:44 +0200

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strlen - return the length of a string.
+* @s: the string that should be lengthed.
+* Return: length of a string as an integer.
+*/
+int _strlen(char *s)
+{
+	printf("%s\n", *s);
+//	return strlen(*s);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - it prints a string.
+* @str: a string to be printed.
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
