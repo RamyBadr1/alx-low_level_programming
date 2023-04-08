3ebf71f1bd0d55967d7a1012c8e740cf7bbab020
RamyBadr1 <ramybadr054@gmail.com>
2023-04-08 23:32:42 +0200

    libmy.a

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_putchar - print c to stdout.
+*
+*Return on success 1.
+*On error, -1 is returned, and errno is set apprpriately.
+*/
+
+int _putchar(char c)
+{
+	putchar(c);	
+	return(0);
+}
