bc4bf5140488fd014cd3c3fb6171a53c0e775495
RamyBadr1 <ramybadr054@gmail.com>
2023-04-11 00:35:34 +0200

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
-	} else
-	{
-		for (i = 1; i < argc; i++)
-			result *= atoi(*(argv + i));
-
-		printf("%d\n", result);
-
-		return (0);
 	}
+
+	for (i = 1; i < argc; i++)
+		result *= atoi(*(argv + i));
+
+	printf("%d\n", result);
+
+	return (0);
+
 }
