43672fdc92efb1a189e4395ef72efdd5c3983813
RamyBadr1 <ramybadr054@gmail.com>
2023-04-12 04:38:38 +0200

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,10 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	s = malloc(sizeof(char) * strlen(str));
+	s = malloc(sizeof(char) * strlen(str) + 1);
+	
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; *(str + i) != '\0'; i++)
 		*(s + i) = *(str + i);
