ce318f1cefbedad4cabef88ee8f24c28d4652509
RamyBadr1 <ramybadr054@gmail.com>
2023-04-12 03:53:20 +0200

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* _strdup - duplicate a string
+* @str: a string to be dublicated.
+* Return: a dublicated string or null
+*/
+char *_strdup(char *str)
+{
+	char *s = malloc(sizeof(int) * strlen(str));
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		*(s + i) = *(str + i);
+
+	return (s);
+}
