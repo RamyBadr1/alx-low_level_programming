f728ba7096443e5100a7fd59f3cbe35226b0e4f3
RamyBadr1 <ramybadr054@gmail.com>
2023-06-15 13:13:27 +0300

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - Return number of all nodes
+ * @h: Pointer to first node
+ * Return: The number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h->next != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
