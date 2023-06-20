d3a5fb6b105937f5474bf2df6865258e8e06b0fc
RamyBadr1 <ramybadr054@gmail.com>
2023-06-20 17:10:21 +0300

    libwinner.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/arthurdamm/holbertonschool-low_level_programming/blob/master/0x17-dynamic_libraries/libwinner.so
-export LD_PRELOAD=../libwinner.so
+wget -P /tmp https://github.com/arthurdamm/holbertonschool-low_level_programming/blob/master/0x17-dynamic_libraries/libwinner.so
+export LD_PRELOAD=/tmp/libwinner.so
